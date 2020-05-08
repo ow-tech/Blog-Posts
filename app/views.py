f6e8c95a672bb45e5f509353762a0af26d7df1b4
ow-tech <wagwanwekon@outloo.com>
2020-05-08 13:04:44 +0300

    creation of login and register routes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from forms import RegistrationForms, LoginForm
 
 
 #dammy post
@@ -26,3 +27,13 @@ def home():
     View root page function that returns the index page and its data
     '''
     return render_template('home.html', blogs = blogs)
+
+@app.route('/register')
+def register():
+    form = RegistrationForms()
+    return render_template('register.html', title= 'Register' form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForms()
+    return render_template('login.html', title= 'login' form=form)
