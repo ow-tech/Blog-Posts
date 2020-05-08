dd5be899e30b6073227e5f5f0e28aaa95cecc85c
ow-tech <wagwanwekon@outloo.com>
2020-05-08 19:44:51 +0300

    adding of validationerror to username and email in registrationform

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User, Pitch
 
 class RegistrationForms(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),Length(min=2,max=20)])
@@ -9,6 +10,16 @@ class RegistrationForms(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username already TAKEN. PLease Choose another')   
+
+    def validate_email(self, email):
+            email = User.query.filter_by(email = email.data).first()
+            if email:
+                raise ValidationError('Email already used. Use a different One') 
 
 class LoginForms(FlaskForm):
     username = StringField('Username', validators=[DataRequired(),Length(min=2,max=20)])
