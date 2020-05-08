9f4a85f64beca035a6b94fa69c0cabe48ef03f3b
ow-tech <wagwanwekon@outloo.com>
2020-05-08 19:01:30 +0300

    adding of more columns in my tables

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from . import db,
+from . import db
 from datetime import datetime
 class Quote:
     '''
@@ -14,14 +14,14 @@ class User (db.Model):
     email = db.Column(db.String(150), unique = True, nullable = False)
     image_file = db.Column(db.String(20), nullable = False, default ='default.jpg')
     password = db.Column(db.String(60), nullable = False)
-    pitches = db.relationship('Pitch', backref='author', lazy= True)
+    posts = db.relationship('Post', backref='author', lazy= True)
 
     def __repr__(self):
         return f'User ("{self.username}","{self.email}","{self.image_file}")'
 
 class Post (db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    # author = db.Column(db.String(50), nullable = False)
+    title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable = False, default= datetime.utcnow)
     content = db.Column(db.Text, nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
