2aa2bd15e174d789b728cc37f34653860fbfe59c
ow-tech <wagwanwekon@outloo.com>
2020-05-08 19:49:45 +0300

    adding of userMixin

--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
-from . import db
+from . import db, login_manager
 from datetime import datetime
+from datetime import datetime
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 class Quote:
     '''
     Quote class to define Quote Objects
@@ -8,7 +15,7 @@ class Quote:
         self.author = author
         self.quote = quote
 
-class User (db.Model):
+class User (db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(50), unique = True, nullable = False)
     email = db.Column(db.String(150), unique = True, nullable = False)
