7835e91c1e9d1a96bf4b05f86a0092008203a5b3
ow-tech <wagwanwekon@outloo.com>
2020-05-12 07:48:58 +0300

    deploying

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ class Config:
     '''
     General configuration parent class
     '''
-    QUOTE_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:alex@localhost/blog'
 
@@ -16,7 +15,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
